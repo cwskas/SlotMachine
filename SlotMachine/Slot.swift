e033b5b0898e142f93563cfa44d1860e811776b1
Willie Strickland <willie@Kepler.local>
2015-04-25 07:42:51 -0500

    3rd commit - create & display slots, spin button works

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Willie Strickland on 4/25/15.
+//  Copyright (c) 2015 NorseStars. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "ACE")
+    var isRed = true
+}
